6d2ec6df8dab2f2aa02fb775f0fc487a619a383d
KhushbooGupta2111 <guptakhushboo81537@gmail.com>
2022-08-17 15:44:23 +0530

    Project Structure Added

---
+++ mycart/shop/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'shop/index.html')
+def about(request):
+    return render(request, 'shop/index.html')
+def contact(request):
+    return render(request, 'shop/index.html')
+def tracker(request):
+    return render(request, 'shop/index.html')
+def search(request):
+    return render(request, 'shop/index.html')
+def productview(request):
+    return render(request, 'shop/index.html')
+def checkout(request):
+    return render(request, 'shop/index.html')
